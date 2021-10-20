e9ca967867e6f326a90075efd5e48beeca51abd5
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-10-20 20:55:06 -0300

    feat: adiciona assets e tema

--- App.tsx
+++ App.tsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 
 import { Home } from "./src/screens/home";
 
 export default function App() {
   return (<Home />);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
