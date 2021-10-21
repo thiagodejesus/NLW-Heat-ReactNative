aa6eb9c0f872028f5c1e893213b832e89f7319e0
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-10-20 22:10:31 -0300

    feat: adiciona avatar image dinamicamente

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Text, View, TouchableOpacity } from "react-native";
 
+import { UserPhoto } from "../UserPhoto";
+
 import { styles } from "./styles";
 
 import LogoSvg from "../../assets/logo.svg";
@@ -10,6 +12,9 @@ export function Header() {
   return (
     <View style={styles.container}>
       <LogoSvg />
+
+      <UserPhoto imageUri="https://github.com/ThiagoDeJesus.png" />
+
       <TouchableOpacity>
         <Text style={styles.logoutText}>Sair</Text>
       </TouchableOpacity>
