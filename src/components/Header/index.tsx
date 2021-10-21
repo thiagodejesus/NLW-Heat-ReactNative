248bec3f82168cc38a2bee779549ccee24a97bdd
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-10-20 22:18:04 -0300

    feat: adiciona borda com linear gradiente no avatarImage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,11 +13,12 @@ export function Header() {
     <View style={styles.container}>
       <LogoSvg />
 
-      <UserPhoto imageUri="https://github.com/ThiagoDeJesus.png" />
-
-      <TouchableOpacity>
-        <Text style={styles.logoutText}>Sair</Text>
-      </TouchableOpacity>
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+        <UserPhoto imageUri="https://github.com/ThiagoDeJesus.png" />
+      </View>
     </View>
   );
 }
--- src/components/UserPhoto/index.tsx
+++ src/components/UserPhoto/index.tsx
@@ -2,9 +2,12 @@ import React from "react";
 
 import { Image } from "react-native";
 
+import { LinearGradient } from "expo-linear-gradient";
+
 import avatarImg from "../../assets/avatar.png";
 
 import { styles } from "./styles";
+import { COLORS } from "../../theme";
 
 const SIZES = {
   SMALL: {
@@ -27,16 +30,30 @@ const AVATAR_DEFAULT = Image.resolveAssetSource(avatarImg).uri;
 export function UserPhoto({ imageUri, sizes = "NORMAL" }: Props) {
   const { containerSize, avatarSize } = SIZES[sizes];
   return (
-    <Image
-      source={{ uri: imageUri || AVATAR_DEFAULT }}
+    <LinearGradient
+      colors={[COLORS.PINK, COLORS.YELLOW]}
+      start={{ x: 0, y: 0.8 }}
+      end={{ x: 0.9, y: 1 }}
       style={[
-        styles.avatar,
+        styles.container,
         {
-          width: avatarSize,
-          height: avatarSize,
-          borderRadius: avatarSize / 2,
+          width: containerSize,
+          height: containerSize,
+          borderRadius: containerSize / 2,
         },
       ]}
-    />
+    >
+      <Image
+        source={{ uri: imageUri || AVATAR_DEFAULT }}
+        style={[
+          styles.avatar,
+          {
+            width: avatarSize,
+            height: avatarSize,
+            borderRadius: avatarSize / 2,
+          },
+        ]}
+      />
+    </LinearGradient>
   );
 }
